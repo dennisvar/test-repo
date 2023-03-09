2210327fed6e1f933f7f2f8d97e5ce8875cdd7de
Dennis Varghese <dennis.varghese@hotmail.com>
2023-03-08 20:46:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstName = 'dennis'
+let lastName = 'varghese'
+
+console.log('hello world')
+
+console.log(firstName + " " + lastName)
+
+let name = 'dennis'
+
+if (name === 'dennis') {
+    console.log("name matches!!??")
+}
+
+let fullName = (fname, lname) => {
+    return `${fname} ${lname}`;
+}
+
+console.log(fullName('joe', 'biden'))
+
+let name1 = fullName('donald', 'trump')
+
+console.log(name1)
